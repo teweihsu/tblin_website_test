903d6c902418df82a5612bd1958f470b95b02ed5
tewei <tewei@teweide-MacBook-Pro.local>
2018-10-25 00:36:27 +0800

    Updates

---
+++ precache-manifest.883cb4a9926c2ea44f8d9464f24be783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61f086effbc9fa1ec1cb7f5d6fe43a91",
+    "url": "/tblin_website_test/static/media/background.61f086ef.png"
+  },
+  {
+    "revision": "f959910d5793451cd2aa",
+    "url": "/tblin_website_test/static/js/runtime~main.f959910d.js"
+  },
+  {
+    "revision": "6588876330f2d2f00343",
+    "url": "/tblin_website_test/static/js/main.65888763.chunk.js"
+  },
+  {
+    "revision": "b764f9124802ffafce5d",
+    "url": "/tblin_website_test/static/js/1.b764f912.chunk.js"
+  },
+  {
+    "revision": "6588876330f2d2f00343",
+    "url": "/tblin_website_test/static/css/main.3ca7be6c.chunk.css"
+  },
+  {
+    "revision": "ef3143b8eb765840225d26bd5f9f6065",
+    "url": "/tblin_website_test/index.html"
+  }
+];
